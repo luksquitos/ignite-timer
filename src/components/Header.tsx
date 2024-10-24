e7710df40bb4dfb1050eead99573e22440ad9981
lucas <lucasmartinscaet@hotmail.com>
2024-10-24 14:25:10 -0300

    feat: finalizando o Header com o NavLinks do React router dom

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,29 @@
+import { NavLink } from "react-router-dom"
 import IgniteLogo from "../assets/Logo.svg"
 import { Timer, Scroll } from "@phosphor-icons/react"
+
+
+//Decidi estilizar isso no globals.css com o .active
+//Mas esta seria uma outra forma de fazer.
+{/* <NavLink to="/home" className="group">
+    <span className="group-[.active]:underline">Home</span>
+</NavLink> */}
+
+//TODO Tooltip
+//https://www.radix-ui.com/primitives/docs/components/tooltip
+
 
 export function Header(){
   return (
     <header className="flex justify-between">
       <img src={IgniteLogo} alt="#" />
       <nav className="flex gap-2">
-        <a href="#" className="size-12"><Timer size={24} className="text-white text-xs"/></a>
-        <a href="#" className="size-12"><Scroll size={24} className="text-white"/></a>
+        <NavLink to={"/"} className="size-12 flex justify-center items-center border-b-[3px] border-transparent hover:border-green"> 
+          <Timer size={24} className="text-gray7"/>
+        </NavLink>
+        <NavLink to={"/history"} className="flex justify-center items-center size-12 border-b-[3px] border-transparent hover:border-green"> 
+          <Scroll size={24} className="text-gray7 "/>
+        </NavLink>
       </nav>
     </header>
   )
