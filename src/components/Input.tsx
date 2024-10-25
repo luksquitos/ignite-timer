48cafa65cd2394fe0850e82849e880553caea5da
lucas <lucasmartinscaet@hotmail.com>
2024-10-25 11:20:38 -0300

    feat: finalizando Input com tudo estático

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,18 +1,23 @@
 export function Input(){
   return(
-    <form className="flex justify-center items-center mt-[72px]">
+    // André mencionou sobre colocar esse form envolta de tudo.
+    // Porque pode ser reaproveitado para o Botão
+    <form className="flex justify-center items-center mt-[72px] gap-2">
       <p>Vou trabalhar em</p>
       <input 
-        className="bg-transparent w-64 text-white px-2 border-b-2 border-b-gray4 placeholder:text-gray4 flex justify-center text-center"
-        placeholder="Dê um nome para o seu projeto" /
-      >
+        className="flex justify-center text-center bg-transparent w-64 text-gray7 px-2 border-b-2 border-b-gray4 placeholder:text-gray4 focus:outline-none focus:border-b-gray7 "
+        placeholder="Dê um nome para o seu projeto" 
+        />
       <p>durante</p>
-      <div className="flex justify-center items-center">
+      <div className="flex justify-center items-center border-b-2 border-b-gray4 gap-2">
         <button className="text-white">-</button>
-        <p>00</p>
+        <input 
+          className="w-5 bg-transparent flex justify-center text-center focus:outline-none text-gray7"
+          placeholder="00"
+        />
         <button className="text-white">+</button>
-        <p>minutos.</p>
       </div>
+      <p>minutos.</p>
     </form>
   )
 }
