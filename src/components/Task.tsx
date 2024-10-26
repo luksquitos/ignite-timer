bd728c7cf150dc36dd6661502fdc5e2c43139f01
lucas <lucasmartinscaet@hotmail.com>
2024-10-26 14:59:44 -0300

    feat: taskboard

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -2,8 +2,11 @@
 
 export function Task(){
   return (
-    <div className="">
-    
+    <div className="mt-1 bg-[#29292e] h-[54px] grid grid-cols-9">
+      <p className="flex items-center col-span-3 pl-6 overflow-hidden">Conserto de ronaldo ronaldo</p>
+      <p className="flex items-center ">25 minutos</p>
+      <p className="flex items-center col-span-3">Há cerca de 2 meses</p>
+      <p className="flex items-center pr-6 col-span-2">Em andamento</p>
     </div>
   )
 }
--- src/components/TaskBoard.tsx
+++ src/components/TaskBoard.tsx
@@ -1,22 +1,41 @@
+import { Task } from "./Task";
+
+
 export function TaskBoard(){
   return (
-    <table className="mt-8 w-full border-collapse overflow-hidden">
-      <thead className="bg-gray3 rounded-tl-lg">
-        <tr className="text-gray7 h-14 ">
-          <th className="text-start pl-6">Tarefa</th>
-          <th className="text-start">Duração</th>
-          <th className="text-start">Início</th>
-          <th className="text-start pr-6">Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr className="text-white">
-          <td>Conserto de ronaldos</td>
-          <td>25 minutos</td>
-          <td>Há cerca de 2 meses</td>
-          <td>EM andamento</td>
-        </tr>
-      </tbody>
-    </table>
+    <section>
+      <header className="mt-8 px-6 w-full h-[54px] bg-gray3 text-gray7 grid grid-cols-9 rounded-tl-lg rounded-tr-lg">
+        <p className="flex items-center col-span-3">Tarefa</p>
+        <p className="flex items-center ">Duração</p>
+        <p className="flex items-center col-span-3">Início</p>
+        <p className="flex items-center col-span-2">Status</p>
+      </header>
+      <section className="w-full h-[408px] overflow-y-auto">
+        {Array.from({length:10}).map(task=><Task/>)} 
+      </section>
+    </section>
   )
 }
+
+//Desisti de mexer com a Table :(
+//
+// export function TaskBoard(){
+//   return (
+//     <table className="mt-8  border-collapse ">
+//       <thead className="">
+//         <tr className="w-full grid grid-cols-6 text-gray7 h-14 ">
+//           <th className="flex items-center bg-gray3 rounded-tl-lg pl-6 col-span-3 ">Tarefa</th>
+//           <th className="flex items-center bg-gray3">Duração</th>
+//           <th className="flex items-center bg-gray3">Início</th>
+//           <th className="flex items-center bg-gray3 rounded-tr-lg pr-6">Status</th>
+//         </tr>
+//       </thead>
+//       <tbody className=" h-[25.5rem] overflow-y-auto">
+//         <div className="w-full border-2">
+
+//           {Array.from({length:10}).map(task=><Task/>)} 
+//         </div>
+//       </tbody>
+//     </table>
+//   )
+// }
