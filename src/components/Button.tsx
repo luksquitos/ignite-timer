e14feb9aea1a6df40b1c745b21ac9ce5ab0c8216
lucas <lucasmartinscaet@hotmail.com>
2024-10-29 14:11:40 -0300

    feat: finalizando o Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,7 @@ import { Play } from "@phosphor-icons/react"
 // Estava apenas testando o tailwind
 export function Button(){
   return (
-    <button className="mt-[76px] w-full h-12 flex justify-center items-center gap-2 bg-green-light rounded-lg text-gray7">
+    <button className="mt-[76px] w-full h-12 flex justify-center items-center gap-2 bg-green rounded-lg text-gray7 hover:bg-green-dark disabled:opacity-70 disabled:cursor-not-allowed disabled:hover:bg-green">
       <Play />
       <p>Começar</p>
     </button>
