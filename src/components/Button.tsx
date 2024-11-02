4839057eb8de5bacca0def41421c808aa45d3fd3
lucas <lucasmartinscaet@hotmail.com>
2024-11-02 13:52:26 -0300

    chore: tipagem para ...props no Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,7 +6,7 @@ const ButtonVariants = {
 }
 
 // Estava apenas testando o tailwind
-export function Button({color, ...props}: {color: "green" | "red" }, ){
+export function Button({color, ...props}: {color: "green" | "red" ; [key: string]: any} ){
   return (
     <button {...props} className={`mt-[76px] w-full h-12 flex justify-center items-center gap-2 rounded-lg ${ButtonVariants[color]} text-gray7  disabled:opacity-70 disabled:cursor-not-allowed `}>
       { color == "green" ? <Play size={24}/> : <HandPalm size={24}/>}
