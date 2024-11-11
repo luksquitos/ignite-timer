0cb05d6c19bfb1f74da1e460867a370aeb8903cc
lucas <lucasmartinscaet@hotmail.com>
2024-11-10 22:00:08 -0300

    feat: iniciando um novo ciclo com useState

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,7 @@ import { Timer } from "../components/Timer";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as zod from 'zod';
+import { useState } from "react";
 
 
 const createNewTaskValidationSchema = zod.object({
@@ -12,6 +13,12 @@ const createNewTaskValidationSchema = zod.object({
     .min(1, "O número mínimo é 1")
     .max(60, "O número máximo é 60")
 })
+
+interface Task {
+  "id": string, // 
+  "description": string,
+  "minutesAmount": number,
+}
 
 // interface newTaskFormData {
 //   "task": string,
@@ -28,10 +35,20 @@ export function Home(){
       "numberInput": 0
     }
   });
+  const [tasks, setTask] = useState<Task[]>([])
+  const [activeTask, setActiveTask] = useState<Task | null>(null)
   const buttonDisabled = !watch("task")
   
   function createNewTask(data: newTaskFormData){
-    console.log(data)
+    const id = String(new Date().getTime())
+    const newTask: Task = {
+      "id": id,
+      "description": data.task,
+      "minutesAmount": data.numberInput
+    }
+    setActiveTask(newTask)
+    // setTask([...tasks, newTask]) // Não garante o estado atual do objeto.
+    setTask((state) => [...state, newTask]) // Garante o estado atual do objeto
     reset()
   }
 
